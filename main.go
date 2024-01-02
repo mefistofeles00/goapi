72122356ea1090c5cc726272c0316b0d5f2a5bcd
mefistofeles00 <0mefistofeles0@gmail.com>
2024-01-03 00:31:44 +0300

    added new functions with choices

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"goapi/amazon"
 	"goapi/trendyol"
+	"os"
 )
 
 func main() {
-	trendyol.Trendyol()
+	var choice int
+	fmt.Println("1. Trendyol Bestseller")
+	fmt.Println("2, Amazon bestsellers")
+	fmt.Scan(&choice)
+
+	switch choice {
+	case 1:
+		trendyol.Trendyol()
+	case 2:
+		amazon.Amazon()
+	default:
+		os.Exit(1)
+
+	}
 }
