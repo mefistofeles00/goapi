bb2eef047d5ca8588c4f226f5909337ebe68c59e
mefistofeles00 <0mefistofeles0@gmail.com>
2024-01-03 00:32:16 +0300

    added amazon bestsellers

--- amazon/amazon.go
+++ amazon/amazon.go
@@ -1,1 +1,22 @@
 package amazon
+
+import (
+	"fmt"
+	"github.com/gocolly/colly/v2"
+	"log"
+)
+
+var p1 = fmt.Println
+
+func Amazon() {
+	c := colly.NewCollector()
+	c.OnHTML(".a-carousel-card", func(e *colly.HTMLElement) {
+		p1(e.Text + "\n")
+	})
+	err := c.Visit("https://www.amazon.com/Best-Sellers/zgbs")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
