f4a35537ecbf85f69c59bf1325f08e2ab363a886
Abdullah inanc <0mefistofeles0@gmail.com>
2024-01-08 23:44:31 +0300

    new func

--- alibaba/alibaba.go
+++ alibaba/alibaba.go
@@ -1,1 +1,21 @@
 package alibaba
+
+import (
+	"fmt"
+	"github.com/gocolly/colly/v2"
+	"log"
+)
+
+var pl = fmt.Println
+
+func Alibaba() {
+	c := colly.NewCollector()
+	c.OnHTML(".multi--outWrapper--SeJ8lrF card--out-wrapper", func(e *colly.HTMLElement) {
+		pl(e.Text + "\n")
+
+	})
+	err := c.Visit("https://www.aliexpress.com/w/wholesale-alibaba-best-sellers.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
