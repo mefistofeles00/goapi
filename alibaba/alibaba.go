d55b5a1208a8d7b3e3b332d89641c8339d491300
Abdullah inanc <0mefistofeles0@gmail.com>
2024-01-10 00:48:10 +0300

    edited functions

--- alibaba/alibaba.go
+++ alibaba/alibaba.go
@@ -2,20 +2,39 @@ package alibaba
 
 import (
 	"fmt"
-	"github.com/gocolly/colly/v2"
+	"github.com/PuerkitoBio/goquery"
 	"log"
+	"net/http"
 )
 
 var pl = fmt.Println
 
 func Alibaba() {
-	c := colly.NewCollector()
-	c.OnHTML(".multi--outWrapper--SeJ8lrF card--out-wrapper", func(e *colly.HTMLElement) {
-		pl(e.Text + "\n")
-
-	})
-	err := c.Visit("https://www.aliexpress.com/w/wholesale-alibaba-best-sellers.html")
+	res, err := http.Get("https://www.aliexpress.com/w/wholesale-alibaba-best-sellers.html")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".list--gallery--C2f2tvm").Each(func(i int, s *goquery.Selection) {
+		// Çeşitli sınıflardan veya etiketlerden verileri çekme
+		title := s.Find(".multi--outWrapper--SeJ8lrF").Text()
+
+		// Verileri ekrana yazdırma
+		pl("\n")
+		fmt.Printf("Ürün #%d:\n", i+5)
+
+		fmt.Printf("Başlık: %s\n", title)
+		pl("\n")
+	})
+
 }
